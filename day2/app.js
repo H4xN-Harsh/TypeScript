e5179d025d64ba5d00d3e73a92b018ede0562783
H4xN-Harsh <harshxvillen74@gmail.com>
2026-01-21 20:29:22 +0530

    day2 of learning typescript

---
+++ day2/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// primitives types
+let a = 5;
+let b = "hey_harsh";
+let c = true;
+// refrence type 
+let arr = [1, 2, 3, 4];
+// tuples 
+let tup = ["harsh", 19];
+//# sourceMappingURL=app.js.map
---
+++ day2/app.ts
@@ -0,0 +1,13 @@
+// primitives types
+let a:number = 5;
+let b:string = "hey_harsh";
+
+let c:boolean = true;
+
+// refrence type 
+let arr:number[]=[1,2,3,4];
+
+
+// tuples 
+
+let tup:[string,number] = ["harsh",19];
